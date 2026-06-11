5a41a7013aee1effa435eb4a5fef597a261211a8
studyHard <2257013699a@gmail.com>
2026-06-11 17:21:48 +0800

    feat: 添加App、School和Student组件及其基本结构

---
+++ Vue2/19_单文件组件/School.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="showName">点我显示学校名称</button>
+  </div>
+</template>
+<script>
+  // 组件交互相关的代码
+export default {
+    name:'School',
+    data() {
+      return {
+        name: '尚硅谷',
+        address: 'beijing',
+      }
+    },
+    methods: {
+      showName() {
+        alert(this.name)
+      }
+    },
+  }
+  
+</script>
+<style>
+  /* 组件样式相关的代码 */
+  .demo{
+    background-color: pink;
+  }
+</style>
---
+++ Vue2/19_单文件组件/main.js
@@ -0,0 +1,9 @@
+import App from "./app.vue";
+
+new Vue({
+  el: '#root',
+  template: `
+    <App></App>
+  `,
+  components: { App }
+})
