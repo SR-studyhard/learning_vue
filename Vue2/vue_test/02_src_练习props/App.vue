8455e7d54ddfc94ae702427182a996916b781115
studyHard <2257013699a@gmail.com>
2026-06-16 11:24:03 +0800

    feat:demo小练习

---
+++ Vue2/vue_test/02_src_练习props/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo">
+    <Student name="张三" :age="18"/>
+  </div>
+</template>
+
+<script>
+  //引入组件
+  import Student from './components/Student.vue';
+  export default {
+    name: 'App',
+    components: {
+        Student
+    }
+  }
+</script>
