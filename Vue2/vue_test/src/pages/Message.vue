47dd724690c2bf72d7657aa8769a9fa89bfc9486
studyHard <2257013699a@gmail.com>
2026-06-25 14:42:15 +0800

    feat: 添加Banner组件，重构路由结构，新增消息和新闻页面

---
+++ Vue2/vue_test/src/pages/Message.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="m in messagelist" :key="m.id">
+        <router-link :to="`/home/message/deatil?id=${m.id}&title=${m.title}`">{{m.title}}</router-link>&nbsp;&nbsp;
+        <button @click="pushShow(m)">push查看</button>
+        <button @click="pushReplace(m)">replace查看</button>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyMessage',
+  data(){
+    return{
+      messagelist: [
+        {id:'01',title:'消息01'},
+        {id:'02',title:'消息02'},
+        {id:'03',title:'消息03'},
+      ]
+    }
+  },
+  methods: {
+    pushShow(m) {
+      this.$router.push({
+        path: '/home/message/deatil',
+        query: {
+          id: m.id,
+          title:m.title
+        }
+      })
+    },
+    pushReplace(m) {
+      this.$router.replace({
+        path: '/home/message/deatil',
+        query: {
+          id: m.id,
+          title:m.title
+        }
+      })
+    }
+  }
+}
+</script>
--- Vue2/vue_test/src/router/index.js
+++ Vue2/vue_test/src/router/index.js
@@ -1,8 +1,14 @@
 // 该文件专门用于创建整个文件的路由器
 import VueRouter from "vue-router";
 //引入组件
-import About from "@/components/About.vue";
-import Home from "@/components/Home.vue";
+import About from "@/pages/About.vue";
+import Home from "@/pages/Home.vue";
+import News from "@/pages/News.vue";
+import Message from "@/pages/Message.vue";
+import Deatil from "@/pages/Deatil.vue";
+Deatil
+Message
+News
 //创建一个路由器
 export default new VueRouter({
   routes: [
@@ -12,7 +18,26 @@ export default new VueRouter({
     },
     {
       path: '/home',
-      component: Home
+      component: Home,
+      children: [
+        {
+          path: 'news',
+          component: News
+        },
+        {
+          path: 'message',
+          component: Message,
+          children: [
+            {
+              path: 'deatil',
+              component: Deatil,
+              props($route) {
+                return { id: $route.query.id, title: $route.query.title }
+              }
+            }
+          ]
+        },
+      ]
     },
   ]
 })
